48e56919cde27c8e75904f7e6e28859f62b9fcad
s.ditaranto <simodita99@gmail.com>
2024-01-25 15:09:12 +0100

    password creata, esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
+// costante che rappresenta le due cifre finali "23"
+const finalNumber = 23;
+
+let firstName = prompt("Come ti chiami?");
+// document.writeln(firstName);
+let lastName = prompt("Qual'è il tuo cognome?");
+// document.writeln(lastName);
+let favouriteColor = prompt("Qual'è il tuo colore preferito?");
+// document.writeln(favouriteColor);
+
+let password = `${firstName}${lastName}${favouriteColor}${finalNumber}`;
+document.getElementById("password").innerHTML = password;
 
 
+document.getElementById("title").innerHTML = "La tua password super sicura è:";
