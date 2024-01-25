d294dbbc29a3af158a39d059044dc463ab055da4
s.ditaranto <simodita99@gmail.com>
2024-01-25 16:04:18 +0100

    stile aggiunto

--- js/script.js
+++ js/script.js
@@ -2,11 +2,8 @@
 const finalNumber = 23;
 
 let firstName = prompt("Come ti chiami?");
-// document.writeln(firstName);
 let lastName = prompt("Qual'è il tuo cognome?");
-// document.writeln(lastName);
 let favouriteColor = prompt("Qual'è il tuo colore preferito?");
-// document.writeln(favouriteColor);
 
 let password = `${firstName}${lastName}${favouriteColor}${finalNumber}`;
 document.getElementById("password").innerHTML = password;
